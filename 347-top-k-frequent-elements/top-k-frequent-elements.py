d4c2c1d3608bd2d52e2d4492f77495f50bf1414c
KONETI MANIKYAM VISHNU PAVAN <kmvishnupavan@gmail.com>
2026-08-01 16:38:05 +0530

    Time: 15 ms (33.29%) | Memory: 20 MB (9.89%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        freq = {}
+
+        # Count frequency
+        for num in nums:
+            freq[num] = freq.get(num, 0) + 1
+
+        # Buckets where index = frequency
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, count in freq.items():
+            buckets[count].append(num)
+
+        result = []
+
+        # Traverse buckets from highest frequency
+        for i in range(len(buckets) - 1, 0, -1):
+            for num in buckets[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
+        
