81eb3cd6f73fdd66a4a2d715f9ed961e8c11160b
KONETI MANIKYAM VISHNU PAVAN <kmvishnupavan@gmail.com>
2026-08-25 18:28:57 +0530

    Time: 64 ms (93.79%) | Memory: 23 MB (96.19%) - LeetSync

---
+++ 146-lru-cache/lru-cache.py
@@ -0,0 +1,62 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def insert(self, node):
+        node.prev = self.tail.prev
+        node.next = self.tail
+
+        self.tail.prev.next = node
+        self.tail.prev = node
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1
+
+        node = self.cache[key]
+
+        self.remove(node)
+        self.insert(node)
+
+        return node.value
+
+    def put(self, key, value):
+        if key in self.cache:
+            node = self.cache[key]
+
+            self.remove(node)
+
+            node.value = value
+            self.insert(node)
+
+            return
+
+        node = Node(key, value)
+        self.cache[key] = node
+        self.insert(node)
+
+        if len(self.cache) > self.capacity:
+            lru = self.head.next
+
+            self.remove(lru)
+            del self.cache[lru.key]
