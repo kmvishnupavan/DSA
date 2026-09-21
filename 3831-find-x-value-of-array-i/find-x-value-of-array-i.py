3eff6da4d0a97e9843159d9b6b2d7aa6a0f0c93b
KONETI MANIKYAM VISHNU PAVAN <kmvishnupavan@gmail.com>
2026-09-21 08:41:04 +0530

    Time: 551 ms (66.67%) | Memory: 21.4 MB (0.00%) - LeetSync

---
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def resultArray(self, nums, k):
+        result = [0] * k
+        dp = [0] * k
+
+        for num in nums:
+            num %= k
+
+            new_dp = [0] * k
+
+            # Start a new subarray
+            new_dp[num] += 1
+
+            # Extend previous subarrays
+            for r in range(k):
+                if dp[r] > 0:
+                    new_r = (r * num) % k
+                    new_dp[new_r] += dp[r]
+
+            # Update the result
+            for r in range(k):
+                result[r] += new_dp[r]
+
+            dp = new_dp
+
+        return result
